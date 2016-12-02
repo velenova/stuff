f08272f77b58b868a3798a8acf83b863844166cb
velenova <velenova@go.byuh.edu>
2016-12-02 12:06:59 -0800

    second commit

---
+++ dispense.h
@@ -0,0 +1,12 @@
+#define DISPENSE_H
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Dispense {
+private:
+    vector<string> products = {"organic fruit bar", "organic exploder bar", "organic sweet rice bar", "organic caramel apple bar", "organic chewy bar", "organic cheesy rice chips", "organic veggie chips", "organic pop chips", "organic plain chips", "organic spearmint gum", "organic mint gum", "organic fruit gum"};
+}
